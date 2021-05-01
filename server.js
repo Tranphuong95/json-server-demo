7fd8fb03be112b35df9a13a894b1552232888bd8
unknown <regitiny@gmail.com>
2021-05-02 02:05:45 +0700

    code token

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+
+const jwt = require('jsonwebtoken')
+const fs = require('fs')
+const bodyParser = require('body-parser')
+
 queryString = require('query-string')
 
 // Set default middlewares (logger, static, cors and no-cache)
@@ -49,6 +54,128 @@ router.render = (req, res) => {
   }
   res.jsonp(res.locals.data)
 }
+
+// login
+// const jsondb = JSON.parse(fs.readFileSync('./db.json', 'UTF-8'))
+const usersdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
+// console.log(jsondb.users)
+//set jwt for login
+const SECRET_KEY = '123456789';
+const expiresIn = '2h'
+
+//create token from a payload
+const createToken = (payload) => {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn })
+}
+
+// server.get('/users', (req, res) => {
+//   const queryParams = queryString.parse(req._parsedUrl.query)
+//   const token = jwt.sign(queryParams, SECRET_KEY, { expiresIn })
+//   console.log(queryParams)
+//   res.send(token)
+// })
+//verify token
+const verifyToken = (token) => {
+  return jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined)
+}
+
+//check if the user exists in database
+const isAuthenticated = ({ email, password }) => {
+  console.log('Email:', email)
+  usersdb.users.findIndex(use => console.log(use.email))
+  return usersdb.users.findIndex(use => use.email === email && use.password === password) !== -1
+}
+
+server.post('/sign-up', (req, res) => {
+  console.log("register endpoint called; request body:");
+  console.log(req.body);
+  const { email, password } = req.body;
+
+  if (isAuthenticated({ email, password }) === true) {
+    const status = 401;
+    const message = 'Email and Password already exist';
+    res.status(status).json({ status, message });
+    return
+  }
+
+  fs.readFile("./users.json", (err, data) => {
+    if (err) {
+      const status = 401
+      const message = err
+      res.status(status).json({ status, message })
+      return
+    };
+
+    // Get current users data
+    var data = JSON.parse(data.toString());
+
+    // Get the id of last user
+    var last_item_id = data.users[data.users.length - 1].id;
+    console.log('last_item_id', last_item_id)
+
+    //Add new user
+    data.users.push({
+      id: last_item_id + 1, email: email, password: password, createdAt: Date.now(),
+      updateAt: Date.now()
+    }); //add some data
+    var writeData = fs.writeFile("./users.json", JSON.stringify(data), (err, result) => {  // WRITE
+      if (err) {
+        const status = 401
+        const message = err
+        res.status(status).json({ status, message })
+        return
+      }
+    });
+  });
+
+  // Create token for new user
+  const access_token = createToken({ email, password })
+  console.log("Access Token:" + access_token);
+  res.status(200).json({ access_token })
+})
+
+// Login to one of the users from ./users.json
+server.post('/login', (req, res) => {
+  console.log("login endpoint called; request body:");
+  console.log(req.body);
+  const { email, password, rememberStatus } = req.body;
+  if (isAuthenticated({ email, password }) === false) {
+    const status = 401
+    const message = 'Incorrect email or password'
+    res.status(status).json({ status, message })
+    return
+  }
+  const access_token = createToken({ email, password })
+  console.log("Access Token:" + access_token);
+  res.status(200).json({ access_token, rememberStatus })
+})
+
+server.use(/^(?!\/auth).*$/, (req, res, next) => {
+  if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+    const status = 401
+    const message = 'Error in authorization format'
+    res.status(status).json({ status, message })
+    return
+  }
+  try {
+    let verifyTokenResult;
+    verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+
+    if (verifyTokenResult instanceof Error) {
+      const status = 401
+      const message = 'Access token not provided'
+      res.status(status).json({ status, message })
+      return
+    }
+    next()
+  } catch (err) {
+    const status = 401
+    const message = 'Error access_token is revoked'
+    res.status(status).json({ status, message })
+  }
+})
+
+
 // Use default router
 const PORT = process.env.PORT || 4001;
 server.use(router)
