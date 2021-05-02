a658213026aed1b159764e3fc921e9c84e1c19ed
unknown <regitiny@gmail.com>
2021-05-02 15:22:50 +0700

    tam thoi bo authorization

--- server.js
+++ server.js
@@ -150,30 +150,31 @@ server.post('/login', (req, res) => {
   res.status(200).json({ access_token, rememberStatus })
 })
 
-server.use(/^(?!\/auth).*$/, (req, res, next) => {
-  if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
-    const status = 401
-    const message = 'Error in authorization format'
-    res.status(status).json({ status, message })
-    return
-  }
-  try {
-    let verifyTokenResult;
-    verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
-
-    if (verifyTokenResult instanceof Error) {
-      const status = 401
-      const message = 'Access token not provided'
-      res.status(status).json({ status, message })
-      return
-    }
-    next()
-  } catch (err) {
-    const status = 401
-    const message = 'Error access_token is revoked'
-    res.status(status).json({ status, message })
-  }
-})
+// authorization cho api
+// server.use(/^(?!\/auth).*$/, (req, res, next) => {
+//   if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+//     const status = 401
+//     const message = 'Error in authorization format'
+//     res.status(status).json({ status, message })
+//     return
+//   }
+//   try {
+//     let verifyTokenResult;
+//     verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+//     console.log('verifyTokenResult: ', verifyTokenResult)
+//     if (verifyTokenResult instanceof Error) {
+//       const status = 401
+//       const message = 'Access token not provided'
+//       res.status(status).json({ status, message })
+//       return
+//     }
+//     next()
+//   } catch (err) {
+//     const status = 401
+//     const message = 'Error access_token is revoked'
+//     res.status(status).json({ status, message })
+//   }
+// })
 
 
 // Use default router
